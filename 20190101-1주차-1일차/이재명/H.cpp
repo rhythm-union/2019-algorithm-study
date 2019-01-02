66622800ebbbd3b4ce015d28e5239bbc49217ba9
jame0313 <jame0313@naver.com>
2019-01-02 17:33:55 +0900

    코드 간결화

--- 20190101-1주차-1일차/이재명/H.cpp
+++ 20190101-1주차-1일차/이재명/H.cpp
@@ -1,19 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-char arr[1000003];
+char arr[1000001];
 int main(){
-	fgets(arr,1000003,stdin);
-	int len=strlen(arr)-1;
-	int mod=len%3,i=0;
-	if(mod==1){
-		printf("%c",arr[0]);
-		i++;
+	scanf("%s",arr);
+	int len=strlen(arr);
+	for(int i=0,sum=0;i<len%3;i++){
+		sum=sum*2+arr[i]-'0';
+		if(i+1==len%3) printf("%d",sum);
 	}
-	if(mod==2){
-		printf("%d",(arr[0]-'0')*2+arr[1]-'0');
-		i+=2;
-	}
-	for(;i<len;i+=3){
+	for(int i=len%3;i+2-len%3<len;i+=3){
 		printf("%d",(arr[i]-'0')*4+(arr[i+1]-'0')*2+arr[i+2]-'0');
 	}
 }
