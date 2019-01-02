ccaeb7941281586d47249af72daedf817d6307ce
jame0313 <jame0313@naver.com>
2019-01-02 19:07:36 +0900

    Update K.cpp

--- 20190101-1주차-1일차/이재명/K.cpp
+++ 20190101-1주차-1일차/이재명/K.cpp
@@ -1,23 +1,7 @@
 #include <stdio.h>
+#define abs(x) ((x)>0?(x):-(x))
 int main(){
 	int w,h,p,q,t;
 	scanf("%d %d %d %d %d",&w,&h,&p,&q,&t);
-	int dp=t%(2*w),dq=t%(2*h);
-	if(dp+p>=w){
-		dp=-(dp-w+p);
-		p=w;
-		if(p+dp<=0){
-		dp=-(dp+w);
-		p=0;
-		}
-	}
-	if(dq+q>=h){
-		dq=-(dq-h+q);
-		q=h;
-		if(q+dq<=0){
-		dq=-(dq+h);
-		q=0;
-		}
-	}
-	printf("%d %d",p+dp,q+dq);
+	printf("%d %d",abs(w-abs(w-p-t%(2*w))),abs(h-abs(h-q-t%(2*h))));
 }
