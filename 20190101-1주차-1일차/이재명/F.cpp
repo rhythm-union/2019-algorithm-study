059ae4cbed7b1c5b5b1db63bfb5bdc72e152531e
jame0313 <jame0313@naver.com>
2019-01-02 17:19:34 +0900

    iostream으로 고침

--- 20190101-1주차-1일차/이재명/F.cpp
+++ 20190101-1주차-1일차/이재명/F.cpp
@@ -1,17 +1,16 @@
-#include <stdio.h>
-#include <algorithm>
+#include <iostream>
+using namespace std;
 int a[10001];
 int main(){
-    int n;
-    scanf("%d",&n);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int n;cin>>n;
     int check;
     for(int i=0;i<n;i++){ 
-        scanf("%d",&check);
+        cin>>check;
         a[check]++;
         }
     for(int i=0;i<=10000;i++){
-        while(a[i]--){
-            printf("%d\n",i);
-        }
+        while(a[i]--) cout<<i<<'\n';
     }
 }
