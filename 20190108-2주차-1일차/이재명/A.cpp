59f37e5a6bf25b4a19e12286444e7e1a3db850c2
이재명 <jame0313@naver.com>
2019-01-09 15:45:03 +0900

    Update A.cpp

--- 20190108-2주차-1일차/이재명/A.cpp
+++ 20190108-2주차-1일차/이재명/A.cpp
@@ -1,10 +1,8 @@
 #include <stdio.h>
-int dp[3];
+int dp[3]={0,1};
 int main(){
 	int N;
 	scanf("%d",&N);
-	dp[0]=0;
-	dp[1]=1;
 	for(int i=2;i<=N;i++) dp[i%3]=dp[(i-1)%3]+dp[(i-2)%3];
 	printf("%d",dp[N%3]);
 }
